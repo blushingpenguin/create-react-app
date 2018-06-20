dc59b3c5765db368447ae004d45c24901b4dfa34
James Bossingham <james.bossingham@vendeq.com>
2018-06-20 16:31:42 +0100

    Add babel polyfill for tests

--- packages/react-scripts/config/polyfills.js
+++ packages/react-scripts/config/polyfills.js
@@ -8,6 +8,8 @@
 // @remove-on-eject-end
 'use strict';
 
+require('babel-polyfill');
+
 if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
   // inconsistent state due to an error, but it gets swallowed by a Promise,
